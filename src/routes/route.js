2a2a4b9ccc4d915c5f5f246976837c2abe5f1aae
gktkumartripathi <gtgaurav1005@gmail.com>
2022-04-11 23:44:09 +0530

    mongodb

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+const BookModel= require("../models/userModel.js")
+router.post("/createBook", async function(req,res){
+    let data = req.body
+    let savedData= await BookModel.create(data)
+    res.send({msg: savedData})
+})
+ 
+router.get("/getBooklist", async function(req,res){
+    let allBooks = await BookModel.find()
+    res.send({msg: allBooks})
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+
 
 module.exports = router;
